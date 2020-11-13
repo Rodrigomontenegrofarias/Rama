f4bf3f25484bde4618aca3993646fab3d0a19a97
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2020-11-13 01:22:39 -0300

    terminado mi wereable

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          supongamos que puedes tocar la ropa y cambiar tu musica
         </p>
         <a
           className="App-link"
