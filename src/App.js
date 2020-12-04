5fa2ef52412075f4acaead7eecb410f8a0b80636
Rodrigo Montenegro <Rodrigo.Montenegro@alumnos.uv.cl>
2020-12-04 05:48:45 -0300

    2

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <p>
           Para la gran universidad de valparaiso!
         </p>
-        <span> nv otro commits </span>
+        <span> nv otro commit </span>
         
       </header>
     </div>
